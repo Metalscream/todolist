66cf5ccb6e104e1fd815f33188ea64f47a258032
Anton <Metalscream96@gmail.com>
2022-04-17 16:36:34 +0300

    fixed eslint config problem

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import { Button } from './Components/Button/Button'; 
-import { ToDoList } from './Components/ToDoList/ToDoList'
+import { Button } from './Components/Button/Button';
+import { ToDoList } from './Components/ToDoList/ToDoList';
 import { useToDoData } from './useToDoData';
 
 function App() {
-
-  const {toDoList, addItem} = useToDoData()
+  const { toDoList, addItem } = useToDoData();
 
   return (
     <div>
-      <ToDoList list={toDoList}/>
+      <ToDoList list={toDoList} />
       <Button onClick={addItem}>Добавить</Button>
     </div>
   );
--- src/Components/Button/Button.tsx
+++ src/Components/Button/Button.tsx
@@ -1,12 +1,12 @@
-import { ReactChild } from 'react'
+import { ReactChild } from 'react';
 
 interface IButtonProps {
   children: ReactChild;
   onClick: () => void;
 }
 
-export function Button({children, onClick}: IButtonProps) {
+export function Button({ children, onClick }: IButtonProps) {
   return (
     <div onClick={onClick}>{children}</div>
-  )
+  );
 }
